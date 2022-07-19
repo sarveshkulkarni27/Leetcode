e9843fb5339cd00bd85366909f7994f241cb58f5
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-07-19 00:58:50 -0400

    Time: 0 ms (100.00%), Space: 41.2 MB (26.87%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,31 @@
 class Solution {
 
     public int uniquePaths(int m, int n) {
-        if(m == 1 && n == 1){
+        // if(m == 1 && n == 1){
+        //     return 1;
+        // }
+        int[][] matrix = new int[m][n];
+        matrix[0][0] = 1;
+        if(m == 1 || n == 1){
             return 1;
         }
-        int[][] matrix = new int[m + 1][n + 1];
         for(int row = 1; row < matrix.length; row++){
             for(int column = 1; column < matrix[0].length; column++){
-                if((row == 1 && column == 2) || (row == 2 && column == 1)){
-                    matrix[row][column] = 1;
-                }else{
+                if(row - 1 == 0 && column - 1 == 0){
+                    matrix[row][column] = 2;
+                }                 
+                else if(row - 1 == 0){
+                    matrix[row][column] = 1 + matrix[row][column - 1];
+                }else if(column - 1 == 0){
+                    matrix[row][column] = 1 + matrix[row - 1][column];
+                }     
+                else{
                     matrix[row][column] = matrix[row - 1][column] + matrix[row][column - 1];
                 }
                 
             }
         }
-        return matrix[m][n];
+        return matrix[m - 1][n - 1];
     }
     
 }
