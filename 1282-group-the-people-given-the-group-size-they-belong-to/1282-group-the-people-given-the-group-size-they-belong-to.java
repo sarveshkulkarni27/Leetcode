555d194360345801f146c30055604242159f0a4d
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-08-26 11:57:17 -0400

    Time: 8 ms (70.18%), Space: 53.9 MB (57.40%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,15 +1,10 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         Map<Integer, List<Integer>> groupMap = new HashMap<>();
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         
         List<List<Integer>> groups = new ArrayList<>();
         
         for(int groupSize = 0; groupSize < groupSizes.length; groupSize++){
-            
-            min = Math.min(min, groupSizes[groupSize]);
-            max = Math.max(max, groupSizes[groupSize]);
             
             List<Integer> groupList = groupMap.getOrDefault(groupSizes[groupSize], new ArrayList<Integer>());
             groupList.add(groupSize);
