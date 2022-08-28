8c5c412a8e2fe1c0f017d49495c2a7a12c32bf3d
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-08-27 22:37:38 -0400

    Time: 1 ms (91.46%), Space: 44.8 MB (42.07%) - LeetHub

---
+++ 667-beautiful-arrangement-ii/667-beautiful-arrangement-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] finalArray = new int[n];
+        
+        int index = 0;
+        boolean front = true;
+        int counter = 0;
+        int nextVal = 0;
+        boolean next = true;
+        while(index < n){          
+            if(k == 1){
+                if(counter == 0){
+                    finalArray[index] = index + 1;
+                }else{
+                    if(next){
+                        finalArray[index] = nextVal++;
+                    }else{
+                        finalArray[index] = nextVal--;
+                    }
+                }                    
+            }
+            else{
+                if(front){
+                    finalArray[index] = (index / 2) + 1;
+                    front = false;
+                }else{
+                    finalArray[index] = n - (index / 2);
+                    front = true;
+                }
+                if(index > 0) k--;
+                if(k == 1){
+                    if(finalArray[index] <= n / 2){
+                        nextVal = finalArray[index] + 1; 
+                    }else{
+                        nextVal = finalArray[index] - 1;
+                       next = false; 
+                    }
+                }
+                counter++;
+                // System.out.println(finalArray[index]);
+            }
+            index++;
+        }
+        return finalArray;
+    }
+}
