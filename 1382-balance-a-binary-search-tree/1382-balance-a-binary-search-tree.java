f14a26eef63707e2a2ef120d0f0d258f12735aae
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-08-28 17:41:25 -0400

    Time: 15 ms (12.60%), Space: 62.3 MB (31.41%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        List<Integer> nodes = new ArrayList<>();
+        traverseTree(root, nodes);
+        
+        Collections.sort(nodes);
+        
+        // TreeNode balancedTree = new TreeNode();
+        return generateBST(nodes, 0, nodes.size() - 1, new TreeNode());
+        // return balancedTree;
+    }
+    
+    public void traverseTree(TreeNode root, List<Integer> nodes){
+        if(root == null){
+            return;
+        }
+        nodes.add(root.val);
+        traverseTree(root.left, nodes);
+        traverseTree(root.right, nodes);
+    }
+    
+    
+    public TreeNode generateBST(List<Integer> nodes, int left, int right, TreeNode balancedTree){
+        if(left > right){
+            return null;
+        }
+        
+        int index = (left + right) / 2;
+        balancedTree = new TreeNode(nodes.get(index));
+        
+        balancedTree.left = generateBST(nodes, left, index - 1, balancedTree);
+        balancedTree.right = generateBST(nodes, index + 1, right, balancedTree);
+        
+        return balancedTree;
+    }    
+}
