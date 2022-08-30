c0ee8241b28d0bd65f4b8f800e66f11b0531e92b
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-08-30 12:30:30 -0400

    Time: 16 ms (12.48%), Space: 44.4 MB (5.69%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> partions = new ArrayList<>();
+        Map<Character, Integer> lastPosition = new HashMap<>();
+        
+        for(int index = 0; index < s.length(); index++){
+            lastPosition.put(s.charAt(index), index);
+        }
+        
+        System.out.println(lastPosition);
+        int currentMax = -1;
+        
+        for(int index = 0; index < s.length(); index++){
+            if(currentMax == -1){
+                if(lastPosition.get(s.charAt(index)) == index){
+                    partions.add(index);
+                }else{
+                    currentMax = Math.max(currentMax, lastPosition.get(s.charAt(index)));
+                }
+            }
+            else if(index == currentMax){
+                partions.add(index);
+                currentMax = -1;
+            }else{
+                currentMax = Math.max(currentMax, lastPosition.get(s.charAt(index)));
+            }
+        }
+        System.out.println(partions);
+        if(partions.size() > 1){
+            int temp = partions.get(0);
+            for(int index = 1; index < partions.size(); index++){
+                int val = partions.get(index) - temp;
+                temp = partions.get(index);
+                partions.set(index, val);
+            }
+        }
+        partions.set(0, partions.get(0) + 1);
+        return partions;
+    }
+}
