4a5b8901f6066d95b1a41f4b3bcd1e2aa4697803
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-09-08 17:52:39 -0400

    Time: 20 ms (40.97%), Space: 54 MB (39.60%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.java
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.java
@@ -1,32 +1,40 @@
 class Solution {
     public int minimumKeypresses(String s) {
-        int[] countArray = new int[26];
-        
-        for(char ch :s.toCharArray()){
+        Integer[] countArray = new Integer[26];
+        Arrays.fill(countArray, 0);
+        for(char ch : s.toCharArray()){
             countArray[ch - 'a']++;
         }
         
-        TreeMap<Integer, Integer> countMap = new TreeMap<>(Collections.reverseOrder());
+        // TreeMap<Integer, Integer> countMap = new TreeMap<>(Collections.reverseOrder());
+        // for(int index = 0; index < 26; index++){
+        //     if(countArray[index] != 0){
+        //         countMap.put(countArray[index], countMap.getOrDefault(countArray[index], 0) + 1);
+        //     }
+        // }
+        Arrays.sort(countArray, (a, b) -> b - a);
+        int buttonPress = 0;
         for(int index = 0; index < 26; index++){
-            if(countArray[index] != 0){
-                countMap.put(countArray[index], countMap.getOrDefault(countArray[index], 0) + 1);
+            if(index < 9){
+                buttonPress += countArray[index];
+            }else if(index < 18){
+                buttonPress += 2 * countArray[index];
+            }else{
+                buttonPress += 3 * countArray[index];
             }
         }
-        
-        int buttonPress = 0;
-        int press = 1;
-        int letterCount = 0;
-        // System.out.println(countMap);
-        for(int key : countMap.keySet()){
-            int currentLetterCount = countMap.get(key);
-            for(int index = letterCount; index < letterCount + currentLetterCount; index++){
-                if(index != 0 && index % 9 == 0){
-                    press++;
-                }
-                 buttonPress += (key * press);
-            }     
-            letterCount += currentLetterCount;
-        }
+        // int press = 1;
+        // int letterCount = 0;
+        // for(int key : countMap.keySet()){
+        //     int currentLetterCount = countMap.get(key);
+        //     for(int index = letterCount; index < letterCount + currentLetterCount; index++){
+        //         if(index != 0 && index % 9 == 0){
+        //             press++;
+        //         }
+        //          buttonPress += (key * press);
+        //     }     
+        //     letterCount += currentLetterCount;
+        // }
         return buttonPress;
     }
 }
