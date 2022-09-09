8a92988a706297234f74aaf1757abfce53b4310b
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-09-09 12:08:26 -0400

    Time: 3 ms (15.76%), Space: 41.1 MB (52.91%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -2,7 +2,6 @@ class Solution {
     public int kthFactor(int n, int k) {
         Stack<Integer> stack1 = new Stack<Integer>();
         Stack<Integer> stack2 = new Stack<Integer>();
-        // Queue<Integer> stack2 = new LinkedList<Integer>();
         Set<Integer> visited = new HashSet<>();
         for(int factor = 1; factor <= n; factor++){
             if(n % factor == 0){
@@ -16,7 +15,6 @@ class Solution {
                 visited.add(val);
                 stack1.push(factor);
                 if(factor != val){
-                    // stack2.add(val);
                     stack2.push(val);
                 }
                 
@@ -25,8 +23,7 @@ class Solution {
                 }
             }
         }
-        System.out.println(stack1);
-        System.out.println(stack2);
+
         if(k <= stack1.size()){
             int diff = stack1.size() - k;
             int returnVal = 0;
@@ -36,12 +33,9 @@ class Solution {
             return returnVal;
         }else if(k <= stack1.size() + stack2.size()){
             int diff = k - stack1.size();
-            System.out.println("Diff: " + diff);
             int returnVal = 0;
             while(diff-- > 0){
-                //returnVal = stack2.poll();
                 returnVal = stack2.pop();
-                System.out.println(returnVal);
             }
             return returnVal;            
         }else{
