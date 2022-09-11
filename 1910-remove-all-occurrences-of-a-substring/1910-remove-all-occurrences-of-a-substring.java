eea362745681cf0d3abc94940e15fb289a49aa79
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-09-10 20:20:15 -0400

    Time: 9 ms (54.42%), Space: 42.6 MB (80.35%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,8 +1,9 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
         while(s.indexOf(part) != -1){
-            int index = s.indexOf(part);
-            s = s.substring(0, index) + s.substring(index + part.length(), s.length());
+            s = s.replaceFirst(part, "");
+            // int index = s.indexOf(part);
+            // s = s.substring(0, index) + s.substring(index + part.length(), s.length());
         }
         return s;
     }
