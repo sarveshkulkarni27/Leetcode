b556b0015d3f71a374419c4c9e4aee655cac70dd
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-09-18 04:13:30 -0400

    Time: 2 ms (99.44%), Space: 42.5 MB (90.98%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,48 +9,28 @@
  * }
  */
 class Solution {
+    int carry = 0;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode finalList = new ListNode();
         ListNode temp = finalList;
         
-        int carry = 0;
+        
         while(l1 != null && l2 != null){
             int answer = l1.val + l2.val + carry;
-            if(answer >= 10){
-                carry = 1;
-                answer -= 10;
-            }else{
-                carry = 0;
-            }
-            finalList.next = new ListNode(answer);
-            finalList = finalList.next;
+            finalList = getNext(finalList, answer);
             l1 = l1.next;
             l2 = l2.next;
         }
         
         while(l1 != null){
             int answer = l1.val + carry;
-            if(answer >= 10){
-                carry = 1;
-                answer -= 10;
-            }else{
-                carry = 0;
-            }
-            finalList.next = new ListNode(answer);
-            finalList = finalList.next;       
+            finalList = getNext(finalList, answer);       
             l1 = l1.next;
         }
         
         while(l2 != null){
             int answer = l2.val + carry;
-            if(answer >= 10){
-                carry = 1;
-                answer -= 10;
-            }else{
-                carry = 0;
-            }
-            finalList.next = new ListNode(answer);
-            finalList = finalList.next; 
+            finalList = getNext(finalList, answer); 
             l2 = l2.next;
         }
         
@@ -59,5 +39,17 @@ class Solution {
         }
         
         return temp.next;
+    }
+    
+    public ListNode getNext(ListNode finalList, int answer){
+        if(answer >= 10){
+            carry = 1;
+            answer -= 10;
+        }else{
+            carry = 0;
+        }
+        finalList.next = new ListNode(answer);
+        finalList = finalList.next;
+        return finalList;
     }
 }
