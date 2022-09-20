22c773aa643da6fa779eae985e2560f199f05363
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-09-20 03:04:49 -0400

    Time: 57 ms (73.62%), Space: 57.2 MB (46.58%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -0,0 +1,56 @@
+class Solution {
+    int maxChainLength = 0;
+    public int longestStrChain(String[] words) {
+        Map<Integer, Set<String>> wordLengthMap = new HashMap<>();
+        
+        int max = -1 * Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(String word : words){
+            int wordLength = word.length();
+            
+            min = Math.min(min, wordLength);
+            max = Math.max(max, wordLength);
+            
+            Set<String> wordList = wordLengthMap.getOrDefault(wordLength, new HashSet<String>());
+            wordList.add(word);
+            wordLengthMap.put(wordLength, wordList);
+        }
+        
+        for(int count = max; count >= min; count--){
+            if(wordLengthMap.containsKey(count)){
+                Set<String> wordList = wordLengthMap.get(count);
+                for(String word : wordList){
+                    for(int index = 0; index < word.length(); index++){
+                        StringBuilder newString = new StringBuilder(word);
+                        newString = newString.deleteCharAt(index);
+                        getDFS(newString, count - 1, min, wordLengthMap, 1);
+                    }                     
+                        // getDFS(new StringBuilder(word), max - 1, min, wordLengthMap, 1);
+                }
+                
+            }
+        }
+        return maxChainLength;
+    }
+    
+    public void getDFS(StringBuilder word, int current, int min, Map<Integer, Set<String>> wordLengthMap, int count){
+        // System.out.println(word.toString() + " " + current + " " + min + " " + wordLengthMap + " " + count);
+        maxChainLength = Math.max(maxChainLength, count); 
+        if(current < min){
+            return;
+        }
+        if((count + current - min) < maxChainLength){
+            return;
+        }
+        if(wordLengthMap.containsKey(current)){
+            Set<String> wordList = wordLengthMap.get(current);
+            if(wordList.contains(word.toString())){
+                for(int index = 0; index < word.length(); index++){
+                    StringBuilder newString = new StringBuilder(word);
+                    newString = newString.deleteCharAt(index);
+                    getDFS(newString, current - 1, min, wordLengthMap, count + 1);
+                }                
+            }
+        }        
+    }
+}
