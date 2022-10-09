463dd5326f648eca2b4766743121573c75260ac4
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-10-09 01:56:39 -0400

    Time: 23 ms (41.31%), Space: 52.2 MB (37.91%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,14 +1,14 @@
 class Solution {
     public String convert(String s, int numRows) {
-        Map<Integer, String> zigZagMap = new HashMap<>();
+        Map<Integer, StringBuilder> zigZagMap = new HashMap<>();
         for(int index = 1; index <= numRows; index++){
-            zigZagMap.put(index, new String());
+            zigZagMap.put(index, new StringBuilder());
         }
         
         int directionFlag = -1;
         int row = 1;
         for(char ch : s.toCharArray()){
-            zigZagMap.put(row, zigZagMap.get(row) + ch);
+            zigZagMap.put(row, zigZagMap.get(row).append(ch));
             if(numRows != 1){
                 if(row == 1 || row == numRows){
                     directionFlag *= -1;
