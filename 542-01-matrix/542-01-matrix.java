bad9f1c3bf0bee709c40413cd47cb29cc6e7d545
Sarvesh Kulkarni <48493502+sarveshkulkarni27@users.noreply.github.com>
2022-10-09 21:01:35 -0400

    Time: 22 ms (62.28%), Space: 73.8 MB (25.75%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -11,7 +11,6 @@ class Solution {
                 if(mat[row][column] == 0){
                     queue.add(new int[]{row, column});
                     finalAnswer[row][column] = 0;
-                    // getBFS(mat, row, column, finalAnswer);
                 }
             }
         }
@@ -35,48 +34,4 @@ class Solution {
         }        
         return finalAnswer;
     }
-    
-//     public void getBFS(int[][] mat, int row, int column, int[][] finalAnswer){
-//         int[][] neighbors = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        
-//         Queue<int[]> queue = new LinkedList<int[]>();
-//         // boolean[][] visited = new boolean[mat.length][mat[0].length];
-//         queue.add(new int[]{row, column, 0});
-//         // visited[row][column] = true;
-//         int minDistance = Integer.MAX_VALUE;
-//         boolean flag = false;        
-//         while(!queue.isEmpty()){
-//             int[] currentCell = queue.poll();
-            
-//             for(int[] neighbor : neighbors){
-//                 int newX = currentCell[0] + neighbor[0];
-//                 int newY = currentCell[1] + neighbor[1];
-//                 if(newX >= 0 && newX < mat.length && newY >= 0 && newY < mat[row].length){
-//                     int currentDistance = currentCell[2] + 1;
-//                     if(currentDistance >= minDistance){
-//                         finalAnswer[row][column] = minDistance;
-//                         flag = true;
-//                         break;
-//                     }
-//                     if(mat[newX][newY] == 0){                       
-//                         if(minDistance == Integer.MAX_VALUE || currentDistance < minDistance){
-//                             finalAnswer[row][column] = currentDistance;                            
-//                         }else{
-//                             finalAnswer[row][column] = minDistance;
-//                         }
-//                         flag = true;
-//                         break;                        
-//                     }
-//                     if(finalAnswer[newX][newY] != 0){
-//                         minDistance = Math.min(minDistance, finalAnswer[newX][newY] + currentDistance);                        
-//                     }
-//                     queue.add(new int[]{newX, newY, currentDistance});
-//                 }
-
-//             }
-//             if(flag){
-//                 break;
-//             }
-//         }
-//     }
 }
